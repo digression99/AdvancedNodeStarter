db743002dfb86699557a1a6ca91cb80b3942a46f
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-05-26 23:51:18 +0900

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI : 'mongodb://iamroot:root1234@ds163226.mlab.com:63226/node-adv-dev',
   cookieKey: '123123123'
 };
